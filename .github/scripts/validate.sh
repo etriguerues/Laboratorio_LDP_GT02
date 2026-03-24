4c15c022c01232c267fc411325d95d3389144a8e
Erick Trigueros <59934930+ErickTrigueros@users.noreply.github.com>
2026-03-23 22:05:00 -0600

    Carga Lab2

--- .github/scripts/validate.sh
+++ .github/scripts/validate.sh
@@ -18,7 +18,9 @@ echo -e "Archivo detectado: ${YELLOW}$FILE_PY${NC}\n"
 # --- PASO 1: CONSTANTES Y DICCIONARIOS ---
 echo -e "${YELLOW}PASO 1: Verificando Diccionario y Constantes...${NC}"
 if grep -qE "TASA_IMPUESTO\s*=\s*0\.12" "$FILE_PY"; then echo -e "${GREEN}[OK] Constante de impuesto correcta.${NC}"; else echo -e "${RED}[ERROR] Falta TASA_IMPUESTO = 0.12.${NC}"; FAILED=1; fi
-if grep -qE "empleado\s*=\s*\{" "$FILE_PY" && grep -q "salario_final"; then echo -e "${GREEN}[OK] Diccionario 'empleado' detectado.${NC}"; else echo -e "${RED}[ERROR] Falta el diccionario 'empleado' con 'salario_final'.${NC}"; FAILED=1; fi
+
+# AQUÍ ESTÁ LA LÍNEA CORREGIDA:
+if grep -qE "empleado\s*=\s*\{" "$FILE_PY" && grep -q "salario_final" "$FILE_PY"; then echo -e "${GREEN}[OK] Diccionario 'empleado' detectado.${NC}"; else echo -e "${RED}[ERROR] Falta el diccionario 'empleado' con 'salario_final'.${NC}"; FAILED=1; fi
 
 # --- PASO 2: FUNCIONES Y CASTING ---
 echo -e "\n${YELLOW}PASO 2: Verificando Funciones y Casting Fuerte...${NC}"
